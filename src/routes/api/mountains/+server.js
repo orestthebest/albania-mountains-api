773eeed8d189e3248bbf419f4de75de23e52eafd
Orest <bashiorest4@gmail.com>
2026-03-12 16:05:17 +0100

    Add GET /api/mountains endpoint updated

--- src/routes/api/mountains/+server.js
+++ src/routes/api/mountains/+server.js
@@ -1,12 +1,10 @@
-import { json } from '@sveltejs/kit';
 import pool from '$lib/server/db';
+import { checkAuth } from '$lib/server/auth';
 
 
 export async function GET() {
 
-  const [rows] = await pool.query(
-    "SELECT * FROM mountains"
-  );
+    const [rows] = await pool.query('SELECT * FROM mountains');
 
-  return json(rows, { status: 200 });
+    return Response.json(rows, { status: 200 });
 }
