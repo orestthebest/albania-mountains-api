f36a5814baa18727bdf505c81eb5960e57b2ed60
Orest <bashiorest4@gmail.com>
2026-03-18 10:46:21 +0100

    Added comments to mountain endpoints (GET,PUT,DELETE)

--- src/routes/api/mountains/[id]/+server.js
+++ src/routes/api/mountains/[id]/+server.js
@@ -2,15 +2,18 @@ import { json } from '@sveltejs/kit';
 import pool from '$lib/server/db';
 import { checkAuth } from '$lib/server/auth';
 
+// GET a single mountain by ID
 export async function GET({ params }) {
 
   const id = params.id;
 
+  // query database for mountain with this ID
   const [rows] = await pool.query(
     "SELECT * FROM mountains WHERE id = ?",
     [id]
   );
 
+  // if no mountain found then return 404 not found
   if (rows.length === 0) {
     return json(
       { message: "Mountain not found" },
@@ -18,11 +21,15 @@ export async function GET({ params }) {
     );
   }
 
+  // return the found mountain status code 200 OK
   return json(rows[0], { status: 200 });
 }
 
+
+// UPDATE a mountain
 export async function PUT({ params, request }) {
 
+    // check if user is authorized
     if (!checkAuth(request)) {
         return Response.json(
             { message: 'Unauthorized' },
@@ -32,8 +39,10 @@ export async function PUT({ params, request }) {
 
     const { id } = params;
 
+     // get data from request body
     const { name, location, type, height, region } = await request.json();
 
+    // check if required fields are missing
     if (!name || !location || !type || !height) {
         return Response.json(
             { message: 'Missing required fields' },
@@ -41,11 +50,13 @@ export async function PUT({ params, request }) {
         );
     }
 
+    // update mountain in database
     const [result] = await pool.query(
         'UPDATE mountains SET name=?, location=?, type=?, height=?, region=? WHERE id=?',
         [name, location, type, height, region, id]
     );
 
+    // if no rows affected then mountain not found
     if (result.affectedRows === 0) {
         return Response.json(
             { message: 'Mountain not found' },
@@ -53,12 +64,14 @@ export async function PUT({ params, request }) {
         );
     }
 
+    // success response     
     return Response.json(
         { message: 'Mountain updated' },
         { status: 200 }
     );
 }
 
+//DELETE a mountain
 export async function DELETE({ params, request }) {
 
     if (!checkAuth(request)) {
@@ -70,6 +83,7 @@ export async function DELETE({ params, request }) {
 
     const { id } = params;
 
+    // delete mountain with specific ID from database 
     const [result] = await pool.query(
         'DELETE FROM mountains WHERE id=?',
         [id]
@@ -82,5 +96,6 @@ export async function DELETE({ params, request }) {
         );
     }
 
+    // success, no content returned
     return new Response(null, { status: 204 });
 }
